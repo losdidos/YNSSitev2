91da7de00d817340c030b719e4060020458ae2ad
losdidos <ytlosdidios@gmail.com>
2026-09-02 17:34:06 +0200

    proxy deployement fix ?

--- src/proxy.ts
+++ src/proxy.ts
@@ -54,6 +54,7 @@ export function proxy(request: NextRequest) {
 
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-nonce', nonce);
+  requestHeaders.set('Content-Security-Policy', csp);
 
   const response = NextResponse.next({ request: { headers: requestHeaders } });
   response.headers.set('Content-Security-Policy', csp);
